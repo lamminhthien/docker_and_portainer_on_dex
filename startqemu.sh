bafa5ce427b77e033cfc14c1a7403e31e1825690
Thiện Lâm <lamminhthien02012000@gmail.com>
2025-09-30 06:54:31 +0700

    Increase CPU, Increase RAM, add more ports

--- startqemu.sh
+++ startqemu.sh
@@ -1,6 +1,9 @@
-qemu-system-x86_64 -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 \
+qemu-system-x86_64 -machine q35 -m 1560 -smp cpus=4 -cpu qemu64 \
 -drive if=pflash,format=raw,read-only,file=$PREFIX/share/qemu/edk2-x86_64-code.fd \
 -netdev user,id=n1,\
 hostfwd=tcp::9000-:9000,\
+hostfwd=tcp::9001-:9001,\
+hostfwd=tcp::9002-:9002,\
+hostfwd=tcp::9003-:9003,\
 hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 \
 -nographic alpine.img
